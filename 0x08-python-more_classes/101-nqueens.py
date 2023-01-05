cbc19651bbd183cb0166f5db5939c77b46bc9640
genesisz <genesiscostantine@gmail.com>
2023-01-05 01:13:26 +0000

    2023

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
+"""Solves the N-queens puzzle.
 
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
@@ -13,6 +29,13 @@ def init_board(n):
 
 
 def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+
+def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
     for r in range(len(board)):
@@ -25,8 +48,10 @@ def board_deepcopy(board):
 
 def xout(board, row, col):
     """X out spots on a chessboard.
+
     All spots where non-attacking queens can no
     longer be played are X-ed out.
+
     Args:
         board (list): The current working chessboard.
         row (int): The row where a queen was last played.
@@ -76,6 +101,7 @@ def xout(board, row, col):
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
+
     Args:
         board (list): The current working chessboard.
         row (int): The current working row.
@@ -111,5 +137,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])    for sol in solutions:
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
         print(sol)
